957151217088bf116716bd00c8de44d6a8984019
anaelenafdez <anaelenafdez@gmail.com>
2025-09-11 10:17:42 +0200

    Saved original string as items of a list

--- reverse_words.py
+++ reverse_words.py
@@ -9,4 +9,22 @@ Waiting:2. reverse_sentence("push commit git") should return "git commit push".
 Waiting:3. reverse_sentence("npm  install   apt    sudo") should return "sudo apt install npm".
 Waiting:4. reverse_sentence("import    default   function  export") should return "export function default import".
 """
+def reverse_sentence(sentence):
 
+    words_array=[]
+    
+    words = sentence.split(' ')
+    for word in words:
+        print(f"{word}")
+        words_array.append(word)
+        print(f"{words_array}")   
+
+    #inverted_sentence = print(f"{word[i]} ")
+
+    #return inverted_sentence
+
+
+reverse_sentence("world hello")
+#reverse_sentence("push commit git")
+#reverse_sentence("npm  install   apt    sudo")
+#reverse_sentence("import    default   function  export")
