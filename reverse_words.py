e10ce59be9534da0fea32bd84ba6af125d528038
anaelenafdez <anaelenafdez@gmail.com>
2025-09-11 11:37:14 +0200

    Versión definitiva de reverse_words con código simplificado y código propio entrecomillado

--- reverse_words.py
+++ reverse_words.py
@@ -9,22 +9,48 @@ Waiting:2. reverse_sentence("push commit git") should return "git commit push".
 Waiting:3. reverse_sentence("npm  install   apt    sudo") should return "sudo apt install npm".
 Waiting:4. reverse_sentence("import    default   function  export") should return "export function default import".
 """
-def reverse_sentence(sentence):
+"""def reverse_sentence(sentence):
 
+    #Creamos una nueva variable para almacenar la lista de palabras del string
     words_array=[]
     
-    words = sentence.split(' ')
+    #Eliminamos los dobles espacios
+    #sentence.replace("  ", "")
+    
+    #Separamos las palabras del string por espacios y las almacenamos en la lista words_array
+    words = sentence.split()#Si no se utiliza nada como separador, los espacios se eliminan
+    
     for word in words:
-        print(f"{word}")
         words_array.append(word)
-        print(f"{words_array}")   
 
-    #inverted_sentence = print(f"{word[i]} ")
+    De forma más limpia (todo en una línea):
+    words_array = sentence.split()
+    
+    #Imprimimos la cadena original en una sola línea    
+    print("Original string:", ' '.join(words_array), "\n")
 
-    #return inverted_sentence
+    #Invertimos el orden de las palabras con el método reverse
+    words_array.reverse()
+    
+    #Imprimimos la cadena invertida en una sola línea
+    
+    inverted_sentence = ' '.join(words_array)
+    print("Inverted string:", inverted_sentence, "\n")
+    return inverted_sentence
+
+"""
+
+#Todo el código anterior se podría eliminar y dejar sólo:"""
+
+
+def reverse_sentence(sentence):
+    print("Inverted string:",' '.join(sentence.split()[::-1]), "\n")
+    return ' '.join(sentence.split()[::-1])
+
 
 
 reverse_sentence("world hello")
-#reverse_sentence("push commit git")
-#reverse_sentence("npm  install   apt    sudo")
-#reverse_sentence("import    default   function  export")
+reverse_sentence("push commit git")
+reverse_sentence("npm  install   apt    sudo")
+reverse_sentence("import    default   function  export")
+
