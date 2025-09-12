7514b7f4623742deabc6bb2061a8c5e0b9b1c8f1
anaelenafdez <anaelenafdez@gmail.com>
2025-09-12 02:36:58 +0200

    Solved Jbelmud.py

--- Jbelmud.py
+++ Jbelmud.py
@@ -17,23 +17,28 @@ def jbelmu(text):
 
     set_words = text.split()
 
-    print("\n")
     print(f"Cadena original: {text}")
     print(f"Palabras originales: {set_words}")
 
+    transformed_words_set = []
+
     for character in set_words:
-        letras_intermedias = sorted(character[1:-1])
-        #print(f"{''.join(letras_intermedias)}")
+        if len(character) <= 2:
+            transformed_word = character
+            transformed_words_set.append(transformed_word)
+            print(f"{transformed_words_set}")
+        else:   
+            letras_intermedias = sorted(character[1:-1])            
+            transformed_word = character[0] +''.join(letras_intermedias) + character[-1]
+            transformed_words_set.append(transformed_word)
         
-        print(f"{character[0]}{''.join(letras_intermedias)}{character[-1]}")
+    print(f"Palabras transformadas: {' '.join(transformed_words_set)}\n")
+
+    return ' '.join(transformed_words_set)
 
 
 
-    print("\n")
-
-    #return text
-
-jbelmu("hello world") #  "hello wlord".
-# jbelmu("i love jumbled text") #  "i love jbelmud text".
-# jbelmu("freecodecamp is my favorite place to learn to code") #  "faccdeeemorp is my faiortve pacle to laern to cdoe".
-# jbelmu("the quick brown fox jumps over the lazy dog") #  "the qciuk borwn fox jmpus oevr the lazy dog".
+transformed_words_set = jbelmu("hello world") #  "hello wlord".
+jbelmu("i love jumbled text") #  "i love jbelmud text".
+jbelmu("freecodecamp is my favorite place to learn to code") #  "faccdeeemorp is my faiortve pacle to laern to cdoe".
+jbelmu("the quick brown fox jumps over the lazy dog") #  "the qciuk borwn fox jmpus oevr the lazy dog".
