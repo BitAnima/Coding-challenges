daa0be27823bb9193427453bbad4691fcd7fddaa
anaelenafdez <anaelenafdez@gmail.com>
2025-10-02 10:21:30 +0200

    Solved binary_to_decimal.py

---
+++ binary_to_decimal.py
@@ -0,0 +1,30 @@
+"""Binary to Decimal
+Given a string representing a binary number, return its decimal equivalent as a number.
+
+A binary number uses only the digits 0 and 1 to represent any number.
+To convert binary to decimal, multiply each digit by a power of 2 and add them together.
+Start by multiplying the rightmost digit by 2^0, the next digit to the left by 2^1, and so on.
+Once all digits have been multiplied by a power of 2, add the result together.
+
+For example, the binary number 101 equals 5 in decimal because: 1 * 2^2 + 0 * 2^1 + 1 * 2^0 = 4 + 0 + 1 = 5"""
+
+import math
+
+def to_decimal(binary):
+
+    sum = 0
+
+    for i, digit in enumerate(reversed(binary)):
+        multiplied_digit = int(digit) * pow(2, i)
+        print(multiplied_digit)
+        sum += multiplied_digit
+
+
+    print(f"Total sum: {sum}")
+
+    return sum
+
+to_decimal("101") # 5.
+to_decimal("1010") # 10.
+to_decimal("10010") # 18.
+to_decimal("1010101") # 85.
